f6508aacac082bd627ffd917870ed607d519dcf1
JORGE MIKAEL COUTINHO ALVES <jmca.lic19@uea.edu.br>
2024-06-14 15:56:42 -0300

    pagina de login

---
+++ app/src/main/java/com/example/myapplication/ArrayLoops.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+fun main() {
+
+    val nomes = arrayOf(
+        "Jorge Mikael",
+        "Ana "
+    )
+//
+//    println(nomes[1])
+
+//    var numero = 1
+//    while ( numero <= 5 ){
+//        println("Execultou: $numero")
+//        numero++
+//    }
+
+//    for ( numero in 1..5 ){
+//        println("Exculta: $numero")
+//    }
+
+    for ( nomes in nomes ){
+        println(nomes)
+    }
+
+
+}
--- app/src/main/java/com/example/myapplication/ifElseWhen.kt
+++ app/src/main/java/com/example/myapplication/ifElseWhen.kt
@@ -1,13 +1,30 @@
 package com.example.myapplication
 
+//fun main() {
+//
+//    val notoAluno = 7
+//    val condicao = notoAluno >= 6
+//
+//    if (condicao) {
+//        println("Aprovado")
+//    }
+//
+//
+//}
+
 fun main() {
 
-    val notoAluno = 7
-    val condicao = notoAluno >= 6
-
-    if (condicao) {
-        println("Aprovado")
+    val opcao= 4
+//    println("Cartão de credito")
+//    println("Extrato Bancario")
+//    println("Saldo")
+    when( opcao ) {
+        1 -> println("Cartão de credito")
+        2 -> println("Extrato Bancario")
+        in 3..5 -> println("Saldo")
+        else -> println("Nenhuma opção selecionado")
     }
+
 
 
 }
